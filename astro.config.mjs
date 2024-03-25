806613958cd59a88731f084f6a562c5dcf6860af
vanlam <vanlam1412@gmail.com>
2024-03-25 17:47:57 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: "https://vanlam2001.github.io/",
-    base: 'master',
+    base: 'my-repo',
     integrations: [
         tailwind(),
     ],
